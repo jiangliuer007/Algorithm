e8950c9f11761b1cc7abb0306a9880a1065547ed
jijiameng <jijiameng@tansuotv.com>
2016-11-16 17:35:08 +0800

    insertion_sort

--- meng/Readme.txt
+++ meng/Readme.txt
@@ -1,1 +1,2 @@
-add test.
+insertion_sort.c  
+    page 18         --算法导论（第三版）
---
+++ meng/insertion_sort.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int insertion_sort(int *pArray, int num) {
+    for (int i = 0 + 1; i < num; i++) { // igonore the first element.
+        int itemp = pArray[i];
+
+        int j = i;
+        while (j >= 0) {
+            if ((pArray[j] < pArray[i]) && (pArray[j + 1 ] > pArray[i])) {
+                for (int k = i; k > j + 1; k--) {
+                    pArray[k] = pArray[k - 1];
+                }
+                pArray[j + 1] = itemp;
+
+                break;
+            }
+
+            j--;
+        }
+
+    }
+
+    return 0;
+}
+
+int main(int argc, char *argv[]) {
+    int iArray[] = {1, 3, 7, 5, 2, 4, 11, 9};
+    size_t numItem = sizeof(iArray) / sizeof(iArray[0]);
+
+    insertion_sort(iArray, numItem);
+    
+    // print the sorted array.
+    for (int i = 0; i < numItem; i++) {
+        printf("%d\t", iArray[i]);
+    }
+    printf("\n");
+    
+    exit(0);
+}
